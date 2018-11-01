fa85846cb35b77ad184376e64e58ac2631ccee73
todstoychev <todstoychev@gmail.com>
2018-11-01 07:38:56 +0200

    Base view created

--- control_dock/app.py
+++ control_dock/app.py
@@ -1,15 +1,25 @@
+import os
 import sys
 
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMdiArea
 
 
 class App:
     NAME = 'Control dock'
     VERSION = '0.1.0'
-    ICON = 'resources/icon.png'
+    ICON = "./resources/icon/favicon-16x16.png"
 
     def __init__(self):
         self.__app = QApplication(sys.argv)
+        self.__main_window = QMainWindow()
+        self.__mdi = QMdiArea()
 
     def run(self):
+        path = os.path.dirname(os.path.abspath(__file__))
+        icon = QIcon(path+"/resources/icon/favicon-16x16.png")
+        self.__main_window.setWindowIcon(icon)
+        self.__main_window.setWindowTitle(self.NAME + " v."+self.VERSION)
+        self.__main_window.setCentralWidget(self.__mdi)
+        self.__main_window.show()
         self.__app.exec()
